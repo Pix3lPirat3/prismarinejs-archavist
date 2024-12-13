aa84a40729714b19994b56896077942da76f9d55
Pix3lPirat3 <pix3lpirat3@yahoo.com>
2024-12-13 11:35:24 -0800

    Create sources.js

---
+++ sources.js
@@ -0,0 +1,57 @@
+module.exports = {
+  mineflayer: {
+    file: 'mineflayer.md', // Stored locally to save resources
+    api: 'https://github.com/PrismarineJS/mineflayer/blob/master/docs/api.md#', // External API to plug anchor into
+    headers: [4]
+  },
+  'mineflayer-pathfinder': {
+    file: 'mineflayer-pathfinder.md',
+    api: 'https://github.com/PrismarineJS/mineflayer-pathfinder/blob/master/readme.md#',
+    headers: [3]
+  },
+  'prismarine-windows': {
+    file: 'prismarine-windows.md',
+    api: 'https://github.com/PrismarineJS/prismarine-windows/blob/master/API.md#',
+    headers: [4]
+  },
+  'prismarine-chat': {
+    file: 'prismarine-chat.md',
+    api: 'https://github.com/PrismarineJS/prismarine-chat/blob/master/README.md#',
+    headers: [4]
+  },
+  'prismarine-world': {
+    file: 'prismarine-world.md',
+    api: 'https://github.com/PrismarineJS/prismarine-world/blob/master/docs/API.md#',
+    headers: [3]
+  },
+  'node-vec3': {
+    file: 'node-vec3.md',
+    api: 'https://github.com/PrismarineJS/node-vec3/blob/master/README.md#',
+    headers: [2]
+  },
+  'prismarine-block': {
+    file: 'prismarine-block.md',
+    api: 'https://github.com/PrismarineJS/prismarine-block/blob/master/doc/API.md#',
+    headers: [4]
+  },
+  'prismarine-entity': {
+    file: 'prismarine-entity.md',
+    api: 'https://github.com/PrismarineJS/prismarine-entity/blob/master/README.md#',
+    headers: [4]
+  },
+  'node-minecraft-data': {
+    file: 'node-minecraft-data.md',
+    api: 'https://github.com/PrismarineJS/node-minecraft-data/blob/master/doc/api.md#',
+    headers: [3]
+  },
+  'node-minecraft-protocol': {
+    file: 'node-minecraft-protocol.md',
+    api: 'https://github.com/PrismarineJS/node-minecraft-protocol/blob/master/docs/API.md#',
+    headers: [3]
+  },
+  'bedrock-protocol': {
+    file: 'bedrock-protocol.md',
+    api: 'https://github.com/PrismarineJS/bedrock-protocol/blob/master/docs/API.md#',
+    headers: [3]
+  }
+}
